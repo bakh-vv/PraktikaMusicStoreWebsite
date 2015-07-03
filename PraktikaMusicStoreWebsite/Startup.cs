62127283030024c5916b38e86646571955c974dc
softwareTM <beisenov_b_b@mail.ru>
2015-07-03 19:54:27 +0300

    Created Album and Artist models; added StoreContext database context; added controller for album model via scoffolding (with entity framework). for some reason you should open .cshtml files with WebForms HTML editor, or VS crashes

---
+++ PraktikaMusicStoreWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PraktikaMusicStoreWebsite.Startup))]
+namespace PraktikaMusicStoreWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
