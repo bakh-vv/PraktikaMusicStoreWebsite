07cd8fa1c499c81cdf3ef29f5f71f3865bceed1f
softwareTM <beisenov_b_b@mail.ru>
2015-07-12 13:59:14 +0300

    added some untracked classes

---
+++ PraktikaMusicStoreWebsite/Models/IdentityModels.cs
@@ -0,0 +1,23 @@
+﻿//using AspNet.Identity.MySQL;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+namespace PraktikaMusicStoreWebsite.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+
+    //[DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public System.Data.Entity.DbSet<PraktikaMusicStoreWebsite.Models.Feedback> Feedbacks { get; set; }
+    }
+}
